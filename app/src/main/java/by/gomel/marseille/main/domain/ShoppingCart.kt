b515969a4526b984c97dfa39720c86dfe04ad455
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-12-17 21:23:56 +0300

    Refactor styles

--- app/src/main/java/by/gomel/marseille/main/domain/ShoppingCart.kt
+++ app/src/main/java/by/gomel/marseille/main/domain/ShoppingCart.kt
@@ -2,6 +2,7 @@ package by.gomel.marseille.main.domain
 
 import by.gomel.marseille.main.data.models.Service
 import java.io.Serializable
+import kotlin.math.min
 
 class ShoppingCart : Serializable {
     private val services: MutableSet<Service> = mutableSetOf()
@@ -10,8 +11,17 @@ class ShoppingCart : Serializable {
     fun add(services: MutableList<Service>) = this.services.addAll(services)
     fun clear() = services.clear()
     fun services() = services.toList()
-    fun getTotalAmount(): Pair<Double, Double>
-            = services.map { it.minPrice }.filter { it > 0 }.sum() to services.map { it.maxPrice }.filter { it > 0 }.sum()
+    fun getTotalAmount(): Pair<Double, Double> {
+        var minPrice = 0.0
+        var maxPrice = 0.0
+
+        services.forEach { service ->
+            minPrice += service.minPrice
+            maxPrice += if (service.maxPrice > 0) service.maxPrice else service.minPrice
+        }
+
+        return minPrice to maxPrice
+    }
 
     fun getTotalAmountString() = "${getTotalAmount().first}" +
             if (getTotalAmount().second > 0) " - ${getTotalAmount().second}" else ""
